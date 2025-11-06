dc9c76de3e9556cb0dc787149a4fa63768d79ca3
Sara Monteiro <smonteiro.dev@gmail.com>
2025-11-05 23:34:39 -0300

    criação da pagina curso e ajuste de caminho

--- src/public/pagina_curso/curso.js
+++ src/public/pagina_curso/curso.js
@@ -11,34 +11,13 @@ document.addEventListener('click', (e) => {
     if (!userMenu.contains(e.target)) {
         userMenu.classList.remove('open');
     }
-})
-
-//JANELA MODAL /////////////////////////////////////////////
-
-const modal = document.getElementById("modal");
-const btnAbrir = document.getElementById("cadastro-aluno");
-const btnFechar = document.querySelector(".fechar");
-
-btnAbrir.onclick = () => {
-    modal.style.display = "flex";
-};
-
-btnFechar.onclick = () => {
-    modal.style.display = "none";
-};
-
-// Fechar clicando fora
-window.onclick = (e) => {
-    if (e.target == modal) {
-        modal.style.display = "none";
-    }
-};
+});
 
 function redirecionar(id, destino) {
     const elemento = document.getElementById(id);
     if (elemento) {
         elemento.addEventListener('click', (e) => {
-            e.preventDefault();
+            e.preventDefault(); // impede recarregar a página com o #
             window.location.href = destino;
         });
     }
@@ -47,7 +26,7 @@ function redirecionar(id, destino) {
 redirecionar("inicio", "../Pagina_Inicial/inicio.html");
 redirecionar("instituicao", "../Instituiçao_editar/instituicao2.html");
 redirecionar("cursos", "../pagina_curso/curso.html");
-redirecionar("disciplinas", "");
+redirecionar("turmas", "");
 redirecionar("alunos", "../Pagina_alunos/alunos.html");
 redirecionar("atividades", "../Pagina_atividades/atividades.html");
 redirecionar("conta", "../Minha_Conta/minha_conta.html");
