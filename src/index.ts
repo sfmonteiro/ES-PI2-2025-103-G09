c37ab50578ea1f6c3c49a5873f3fdea4867f1476
GuilhermeM2251 <guilherme.mpc@puccampinas.edu.br>
2025-11-06 13:09:38 -0300

    ajustando o servidor em Typescript

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,13 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import path from "path";
-import { fileURLToPath } from "url";
 
 const app = express();
 const PORT = 3000;
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 app.use(express.json());
-
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", (req, res) => {
+app.get("/", (req: Request, res: Response) => {
   res.redirect("/Login/login.html");
 });
 
