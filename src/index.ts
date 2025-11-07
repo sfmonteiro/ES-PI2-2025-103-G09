6df36b62f55900772f9cc50929bb5270e93b2ef9
GuilhermeM2251 <guilherme.mpc@puccampinas.edu.br>
2025-11-07 10:23:29 -0300

    ajuste no servidor ts

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,13 @@ import path from "path";
 const app = express();
 const PORT = 3000;
 
+const rootDir = path.resolve(__dirname, "..");
+const publicPath = path.join(rootDir, "src", "public");
+
+console.log("Servindo arquivos estáticos de:", publicPath);
+
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(publicPath));
 
 app.get("/", (req: Request, res: Response) => {
   res.redirect("/Login/login.html");
